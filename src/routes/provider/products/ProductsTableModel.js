5826a5d2fa5704ec80a019463ab474876f17a879
augustotakeo <augustotakeo@gmail.com>
2021-10-18 01:26:18 -0300

    Model of the table products

---
+++ src/routes/provider/products/ProductsTableModel.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../../../database');
+
+const ProviderTableModel = require('../ProviderTableModel');
+
+const columns = {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    inventory: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        default: 0
+    },
+    provider_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+            model: ProviderTableModel,
+            key: 'id'
+        }
+    }
+};
+
+const options = {
+    freezeTableName: true,
+    tableName: "products",
+    timestamps: true
+}
+
+module.exports = sequelize.define("products", columns, options);
