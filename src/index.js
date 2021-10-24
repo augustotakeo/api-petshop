51cd71e5f42d16ed0f74b382f9a2358c8149e8a4
augustotakeo <augustotakeo@gmail.com>
2021-10-24 02:54:42 -0300

    Add Access-Control-Allow-Origin in header

--- src/index.js
+++ src/index.js
@@ -27,6 +27,11 @@ app.use( (req, res, next) => {
     }
 })
 
+app.use( (req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    next();
+})
+
 app.use(express.json());
 
 app.use(routes);
