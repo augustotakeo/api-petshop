4dcd7e8b2a2126fa69cadb23e3456d28ed1a57f5
augustotakeo <augustotakeo@gmail.com>
2021-10-24 02:50:49 -0300

    Add methods in the class to load, remove, update, validate and decrease

--- src/routes/provider/products/Product.js
+++ src/routes/provider/products/Product.js
@@ -1,7 +1,9 @@
 const ProductsTable = require('./ProductsTable');
+const DataNotProvided = require('../../../errors/DataNotProvided');
+const InvalidField = require('../../../errors/InvalidField');
 
 class Product {
-    constructor({ id, title, description, price, inventory, provider_id, createdAt, updatedAt }) {
+    constructor({ id, title, description, price, inventory, provider_id, createdAt, updatedAt, version }) {
         this.id = id;
         this.title = title;
         this.description = description;
@@ -10,9 +12,12 @@ class Product {
         this.provider_id = provider_id;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.version = version;
     }
 
     async create() {
+        this.validate();
+
         const data = {
             title: this.title,
             description: this.description,
@@ -25,6 +30,79 @@ class Product {
         this.id = result.id;
         this.createdAt = result.createdAt;
         this.updatedAt = result.updatedAt;
+        this.version = result.version;
+    }
+
+    async load() {
+        const result = await ProductsTable.searchById(this.id, this.provider_id);
+        this.title = result.title;
+        this.description = result.description;
+        this.price = result.price;
+        this.inventory = result.inventory;
+        this.createdAt = result.createdAt;
+        this.updatedAt = result.updatedAt;
+        this.version = result.version;
+    }
+
+    remove() {
+        return ProductsTable.remove(this.id, this.provider_id);
+    }
+
+    update() {
+
+        let data = {};
+        
+        if(typeof this.title == "string" && this.title.length > 0) {
+            data["title"] = this.title;
+        }
+
+        if(typeof this.description == "string" && this.description.length > 0) {
+            data["description"] = this.description;
+        }
+
+        if(typeof this.price == "number" && this.price > 0) {
+            data["price"] = this.price;
+        }
+
+        if(typeof this.inventory == "number") {
+            data["inventory"] = this.inventory;
+        }
+
+        if(Object.keys(data).length == 0){
+            throw new DataNotProvided();
+        }
+
+        return ProductsTable.update(this.id, this.provider_id, data);
+
+    }
+    
+    async decreaseInventory({quantity}) {
+        if(!quantity) {
+            throw new DataNotProvided();
+        }
+        
+        return ProductsTable.decrease(
+            this.id,
+            this.provider_id,
+            "inventory",
+            quantity
+        )
+    }
+
+    validate() {
+        let fields = ["title", "description"];
+        fields.forEach( field => {
+            if( typeof this[field] != "string" || this[field].length == 0 ) {
+                throw new InvalidField(field);
+            }
+        })
+
+        fields = ["price", "inventory"]
+        fields.forEach( field => {
+            if( typeof this[field] != "number" || this[field] <= 0 ) {
+                throw new InvalidField(field);
+            }
+        })
     }
 }
 
