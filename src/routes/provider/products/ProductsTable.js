16669b15985efca837ee7eee86dc7ff90232dd71
augustotakeo <augustotakeo@gmail.com>
2021-10-24 02:51:52 -0300

    Add functions to create, searchById, remove, update and decrease

--- src/routes/provider/products/ProductsTable.js
+++ src/routes/provider/products/ProductsTable.js
@@ -1,23 +1,83 @@
 const Model = require('./ProductsTableModel');
+const sequelize = require('../../../database');
 
 const NotFound = require('../../../errors/NotFound');
+const QuantityTooHigh = require("../../../errors/QuantityTooHigh");
 
 module.exports = {
     list(idProvider) {
         return Model.findAll({
             where: {
                 provider_id: idProvider
+            },
+            raw: true
+        })
+    },
+
+    async create(data) {
+        const product = await Model.create(data);
+
+        if(!product) {
+            throw new NotFound("product");
+        }
+
+        return product;
+    },
+
+    async searchById(idProduct, idProvider) {
+        const product = await Model.findOne({
+            where: {
+                id: idProduct,
+                provider_id: idProvider
+            }
+        });
+
+        if(!product) {
+            throw new NotFound("product");
+        }
+
+        return product;
+    },
+
+    remove(idProduct, idProvider) {
+        return Model.destroy({
+            where: {
+                id: idProduct,
+                provider_id: idProvider
             }
         })
     },
 
-    create(data) {
-        const product = Model.create(data);
+    update(id, provider_id, data) {
+        return Model.update(
+            data,
+            {
+                where:{
+                    id,
+                    provider_id
+                }
+            }
+        )
+    },
 
-        if(!product) {
-            throw new NotFound();
-        }
+    decrease(id, provider_id, field, quantity) {
+        return sequelize.transaction( async transaction => {
+            const product = await Model.findOne({
+                where: {
+                    id,
+                    provider_id
+                }
+            })
 
-        return product;
+            if( product[field] < quantity ){
+                throw new QuantityTooHigh();
+            }
+
+            product[field] -= quantity;
+            
+            await product.save();
+            
+            return product;
+        })
     }
 }
